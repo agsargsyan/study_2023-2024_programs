594f2b307cd23755440c17bfad3f2bd83fc4f3b2
agsargsyan <agsargsyanos@gmail.com>
2024-02-25 17:12:58 +0300

    Update mininet

--- mininet/main.py
+++ mininet/main.py
@@ -37,29 +37,49 @@ class NetworkTopo(Topo):
         		)
         #добавление оконечных устройств
         for i in range(1, 2*num_hosts_subnets + 1):
-            host_ip = '10.0.0.{}/24' if i % 2 == 0 else '10.1.0.{}/24'
-            default_route = 'via 10.0.0.1' if i % 2 == 0 else 'via 10.1.0.1'
+            host_ip = '10.0.0.{}/24' if i % 2 == 1 else '10.1.0.{}/24'
+            default_route = 'via 10.0.0.1' if i % 2 == 1 else 'via 10.1.0.1'
             host = self.addHost(name='h{}'.format(i),
                                 ip=host_ip.format(250 - i),
                                 defaultRoute=default_route)
-            switch = s1 if i % 2 == 0 else s2
+            switch = s1 if i % 2 == 1 else s2
             self.addLink(host, switch, bw = 100)
             
-            
+# Функция для мониторинга очереди
+def monitor_queue(net, interface, interval=1, output_file='queue_monitor.log'):
+    with open(output_file, 'w') as file:
+        try:
+            while True:
+                result = net['r0'].cmd(f'tc -s qdisc show dev {interface}')
+                print(result, file=file)
+                time.sleep(interval)
+        except KeyboardInterrupt:
+            print("Мониторинг завершен")
+ 
+             
 #Функция для запуска iperf между парой хостов
 def run_iperf(net, host1_name, host2_name):
     h1 = net.get(host1_name)
     h2 = net.get(host2_name)
     # Запуск iperf сервера на втором хосте
-    h2.cmd("iperf3 -s -D")
+    h2.cmdPrint("iperf3 -s -D")
     # Запуск iperf клиента на первом хосте, соединяющегося со вторым хостом
     h1.cmd(f"mkdir -p output/{host1_name}_to_{host2_name}")
-    h1.cmd(f'iperf3 -c {h2.IP()} -w 16K -t 26 -J > output/{host1_name}_to_{host2_name}/{host1_name}_to_{host2_name}_iperf3.json')
+    h1.cmdPrint(f'iperf3 -c {h2.IP()} -t 26 -J > output/{host1_name}_to_{host2_name}/{host1_name}_to_{host2_name}_iperf3.json')
     h1.cmd(f"cd output/{host1_name}_to_{host2_name} && plot_iperf.sh {host1_name}_to_{host2_name}_iperf3.json")
-         
-            
-            
-            
+
+
+def monitor_queue(net, interface="r0-eth2", interval=1, output_file='queue_monitor.log'):
+    t = threading.currentThread()
+    with open(output_file, 'w') as file:
+        while getattr(t, "do_run", True):
+            result = net['r0'].cmd(f'tc -s qdisc show dev {interface}')
+            print(result, file=file)
+            time.sleep(interval)
+
+
+
+
 def run(num_hosts_subnets=20):
     topo = NetworkTopo(num_hosts_subnets=20)
     net = Mininet(topo=topo)
@@ -67,8 +87,14 @@ def run(num_hosts_subnets=20):
     # Add routing for reaching networks that aren't directly connected
     info(net['r0'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r0-eth2"))
     info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r1-eth2"))
-    #установка типа окна TCP
-    info(net['r1'].cmd("sudo sysctl -w net.ipv4.tcp_congestion_control=reno"))	
+    #установка типа окна TCP и ее размера
+    info(net['c0'].cmd("sudo sysctl -w net.ipv4.tcp_congestion_control=reno"))
+    info(net['c0'].cmd("sudo sysctl -w net.ipv4.tcp_rmem='2000 8000 16000'"))
+    info(net['c0'].cmd("sudo sysctl -w net.ipv4.tcp_wmem='2000 8000 16000'"))
+
+    
+    
+    
     #установка задержек на соединениях с помощью netem
     for i in range(1, 2*num_hosts_subnets + 1):
 	    host = net.get(f'h{i}')  # Генерируем имя хоста на основе i
@@ -77,7 +103,7 @@ def run(num_hosts_subnets=20):
 	    else:
 	     host.cmd(f"sudo tc qdisc add dev h{i}-eth0 root netem delay 15ms") 
 
-    
+
     #настройка алгоритма RED
     avpkt = 500
     limit = avpkt * 300
@@ -85,12 +111,13 @@ def run(num_hosts_subnets=20):
     max_ = avpkt * 150
     probability = 0.1
     bandwidth = 20
-    burst = 125
-    
-    net['r0'].cmdPrint("tc qdisc add dev r0-eth2 root handle 1: red limit {} min {} max {} avpkt {} bandwidth {} probability {} burst {}".format(limit, min_, max_, avpkt, bandwidth, probability, burst))
+    #burst = 125
+
+    net['r0'].cmdPrint("tc qdisc add dev r0-eth2 root handle 1: red limit {} min {} max {} avpkt {} bandwidth {} probability {}".format(limit, min_, max_, avpkt, bandwidth, probability))
 
     net.start()
     threads = []
+            
     # Пример: запуск iperf между несколькими парами хостов
     for i in range(1, 2*num_hosts_subnets, 2):
         host1 = f'h{i}'
@@ -98,14 +125,21 @@ def run(num_hosts_subnets=20):
         t = Thread(target=run_iperf, args=(net, host1, host2))
         t.start()
         threads.append(t)
+        
+        
+  
     # Ожидание завершения всех потоков
     for t in threads:
         t.join()
-        
+    
+       
+    #Мониторинг параметров RED    
+    print(net['r0'].cmd("tc -s qdisc show dev r0-eth2"))
     #CLI(net)
     net.stop() # Остановка сети
 
-#запуск программи
+
+#запуск программы
 if __name__ == '__main__':
     setLogLevel('info')
     run()
--- mininet/plots.plt
+++ mininet/plots.plt
@@ -1,5 +1,5 @@
-set terminal pdf
-set output 'results/RTT.pdf'
+set terminal postscript eps
+set output 'results/RTT.eps'
 set datafile separator " "
 set xlabel "Time (sec)"
 set ylabel "RTT (sec)"
@@ -13,23 +13,23 @@ plot for [data in FILES] data u 2:($8/1000) with lines linecolor rgb "black" not
 
 
 
-set terminal pdf
-set output 'results/cwnd.pdf'
+set terminal postscript eps
+set output 'results/cwnd.eps'
 set datafile separator " "
 set xlabel "Time (sec)"
-set ylabel "Cwnd"
+set ylabel "Cwnd [pkt]"
 set yrange [0:*]
 set title "Sent Cwnd over time"
 unset key
 set grid
 set style data lines
 FILES = system("ls -1 *.dat")
-plot for [data in FILES] data u 2:7 with lines linecolor rgb "black" notitle
+plot for [data in FILES] data u 2:($7/500) with lines linecolor rgb "black" notitle
 
 
 
-set terminal pdf
-set output 'results/RTT_Var.pdf'
+set terminal postscript eps
+set output 'results/RTT_Var.eps'
 set datafile separator " "
 set xlabel "Time (sec)"
 set ylabel "RTT Var (sec)"
